5dc0955a43428fc45a2b10db1e9ebfcaf546f949
Yakov <ygaberman@gmail.com>
2020-11-24 17:10:47 -0500

    Fixes typings

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,9 @@
 declare module 'cache-money' {
-    export default class Cache {
-        constructor(config: {ttl: number, maxKeys: number})
+    export class Cache {
+        constructor(config?: {
+            ttl: number;
+            maxKeys: number;
+        })
         get(key: string): any;
         set(key: string, value: any);
     }
